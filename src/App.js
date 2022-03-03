7734e29b5751763a253c86001cdf0bbabbcfbbc9
SAUL NJIE <SAULUS92@gmail.com>
2022-03-03 12:48:34 +0100

    First graphql test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_TODOS = gql`
+  query GetTodos {
+    todos {
+      data {
+        id
+        title
+      }
+    }
+  }
+`;
+
+const GET_TODO = gql`
+  query GetTodo($id: ID!) {
+    todo(id: $id) {
+      id
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_TODOS);
+  const todos = data?.todos.data;
+
+  const todo = useQuery(GET_TODO, { variables: { id: "18" } });
+  console.log("todo", todo.data);
+
+  console.log(todos);
+  if (error) {
+    return <p>Shit hit the fan</p>;
+  }
+  if (loading || !todos) {
+    return <p>Is Loading</p>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {todos.map((todo) => {
+          return <p key={todo.id}>{todo.id}</p>;
+        })}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
